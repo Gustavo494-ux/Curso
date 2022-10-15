3525554fd787403b689ae5f7aee176daf0e68e6a
Gustavo Gama <gustavogama494@gmail.com>
2022-10-15 17:21:47 -0300

    Arrays e Slices

---
+++ 9 - Arrays e Slices/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Arrays e Slices")
+
+	//Array
+	var array1 [5]string
+	array1[0] = "Posição 1"
+	fmt.Println(array1)
+
+	array2 := [5]string{"Posição 1", "Posição 2", "Posição 3", "Posição 4", "Posição 5"}
+	fmt.Println(array2)
+
+	array3 := [...]int{1, 2, 3, 4, 5}
+	fmt.Println(array3)
+
+	//Slice
+	slice := []int{1, 2, 3, 4, 5}
+	slice = append(slice, 18)
+	fmt.Println(slice)
+
+	slice2 := array2[1:3]
+	fmt.Println(slice2)
+}
