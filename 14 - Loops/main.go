43f7edc3bbdb45892c72a87276ab99cca877567a
Gustavo Gama <gustavogama494@gmail.com>
2022-10-16 14:22:19 -0300

    14 - loops

---
+++ 14 - Loops/main.go
@@ -0,0 +1,57 @@
+package main
+
+func main() {
+	/*
+			i := 0
+
+		for i < 10 {
+			println("Incrementando i:", i)
+			time.Sleep(time.Second)
+			i++
+		}
+		fmt.Println(i)
+		fmt.Println("----------------------")
+	*/
+	/*
+		for j := 0; j < 10; j++ {
+			println("Incrementando j:", j)
+			time.Sleep(time.Second)
+		}
+	*/
+
+	/*
+		nomes := [3]string{"João", "Davi", "Lucas"}
+		for _, nome := range nomes {
+			fmt.Println(nome)
+		}
+
+		for indice, letra := range "Palavra" {
+			fmt.Println(indice, letra)
+			fmt.Println(indice, string(letra))
+		}
+	*/
+
+	/*	usuario := map[string]string{
+			"nome":      "Gustavo",
+			"sobrenome": "Gama",
+		}
+
+		for chave, valor := range usuario {
+			fmt.Println(chave, valor)
+		}
+	*/
+
+	/*
+		type usuarioStruct struct {
+			nome      string
+			sobrenome string
+		}
+
+		usuario2 := usuarioStruct{"Gustavo", "Gama"}
+
+		//Não é possível utilizar o range em Struct
+		for chave, valo0r := range usuario2 {
+			fmt.Println(chave, valo0r)
+		}
+	*/
+}
