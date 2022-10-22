954999d57413a618c76623332de5f7ce4f9c1fdd
Gustavo Gama <gustavogama494@gmail.com>
2022-10-21 21:12:12 -0300

    Panic e Recover

---
+++ 24 - Panic e Recover/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func recuperarExecucao() {
+	if r := recover(); r != nil {
+		fmt.Println("Execução recuperada com sucesso!")
+	}
+}
+
+func AlunoEstaAprovado(n1, n2 float64) bool {
+	defer recuperarExecucao()
+	media := (n1 + n2) / 2
+
+	if media > 6 {
+		return true
+	} else if media < 6 {
+		return false
+	}
+
+	panic("A média é exatamente igual a 6")
+}
+
+func main() {
+	fmt.Println(AlunoEstaAprovado(6, 6))
+	fmt.Println("Pós Execução")
+	recover()
+}
