70023ff3ac958d3aa81400c89d20bd10ced8638c
Gustavo Gama <gustavogama494@gmail.com>
2022-11-12 17:34:31 -0300

    49 - Json Função Unmarshal

---
+++ Seção 6/49 - Json Função Unmarshal/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type cachorro struct {
+	Nome  string `json:"nome"`
+	Raca  string `json:"raca"`
+	Idade uint   `json:"idade"`
+}
+
+func main() {
+	cachorroEmJSON := `{"nome":"Rex","raca":"Dálmata","idade":3}`
+
+	var c cachorro
+
+	if erro := json.Unmarshal([]byte(cachorroEmJSON), &c); erro != nil {
+		log.Fatal(erro)
+	}
+
+	fmt.Println(c)
+	////////////////////////////////////////////////////////////////////////////////
+	c2 := make(map[string]string)
+	cachorro2EmJSON := `{"nome":"Toby","raca":"Poodle"}`
+
+	if erro := json.Unmarshal([]byte(cachorro2EmJSON), &c2); erro != nil {
+		log.Fatal(erro)
+	}
+
+	fmt.Println(c2)
+}
