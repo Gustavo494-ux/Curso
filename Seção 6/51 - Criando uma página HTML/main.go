11440e45691bae8eeeab1404c5f78dfeab525477
Gustavo Gama <gustavogama494@gmail.com>
2022-11-13 15:06:45 -0300

    52 - Renderizando página HTML

--- Seção 6/51 - Criando uma página HTML/main.go
+++ Seção 6/51 - Criando uma página HTML/main.go
@@ -3,14 +3,29 @@ package main
 import (
 	"log"
 	"net/http"
+	"text/template"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Olá Mundo!"))
+var templates *template.Template
+
+type usuario struct {
+	Nome  string
+	Email string
 }
 
 func main() {
+
 	http.HandleFunc("/home", home)
 
 	log.Fatal(http.ListenAndServe(":5000", nil)) //Servidor já está rodando
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	u := usuario{
+		"João",
+		"joao.pedro@gmail.com",
+	}
+
+	templates = template.Must(template.ParseGlob("*.html"))
+	templates.ExecuteTemplate(w, "home.html", u)
 }
