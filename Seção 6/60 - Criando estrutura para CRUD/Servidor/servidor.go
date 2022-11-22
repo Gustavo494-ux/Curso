cb7b587c7d359b56e2a07658dc30df2bde71cb3b
Gustavo Gama <gustavogama494@gmail.com>
2022-11-22 20:47:30 -0300

    64 - Atualizando Dados

--- Seção 6/60 - Criando estrutura para CRUD/Servidor/servidor.go
+++ Seção 6/60 - Criando estrutura para CRUD/Servidor/servidor.go
@@ -134,3 +134,47 @@ func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 }
+
+// AtualizarUsuario altera os dados de um usuário no banco de dados
+func AtualizarUsuario(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
+
+	ID, erro := strconv.ParseInt(parametros["id"], 10, 32)
+	if erro != nil {
+		w.Write([]byte("Erro ao converter o parâmetro para inteiro"))
+		return
+	}
+
+	corpoRequisicao, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		w.Write([]byte("Erro ao ler o corpo de requisição"))
+		return
+	}
+
+	var usuario usuario
+	if erro := json.Unmarshal(corpoRequisicao, &usuario); erro != nil {
+		w.Write([]byte("Erro ao converter o usuário para struct"))
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		w.Write([]byte("Erro ao conectar com o banco de dados!"))
+		return
+	}
+	defer db.Close()
+
+	statement, erro := db.Prepare("UPDATE USUARIOS SET NOME = ?,EMAIL = ? WHERE ID = ?")
+	if erro != nil {
+		w.Write([]byte("Erro executar o statement"))
+		return
+	}
+	defer statement.Close()
+
+	if _, erro := statement.Exec(usuario.Nome, usuario.Email, ID); erro != nil {
+		w.Write([]byte("Erro ao atualizar o usuário"))
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
