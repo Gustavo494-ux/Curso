5a3644fb9b634da4a6c0678b26dc835213da804f
Gustavo Gama <gustavogama494@gmail.com>
2022-12-05 19:12:06 -0300

    76 - Pacote Repositórios

--- DevBook/api/src/controllers/usuarios.go
+++ DevBook/api/src/controllers/usuarios.go
@@ -1,10 +1,35 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/banco"
+	"api/src/modelos"
+	"api/src/repositorios"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 // CriarUsuario insere um usuário no banco de dados
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Criando Usuário!"))
+	corpoRequest, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	var usuario modelos.Usuario
+	if erro = json.Unmarshal(corpoRequest, &usuario); erro != nil {
+		log.Fatal(erro)
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		log.Fatal(erro)
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
+	repositorio.Criar(usuario)
 }
 
 // BuscarUsuarios busca todos os usuários salvos no banco de dados
@@ -17,12 +42,12 @@ func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Buscando um Usuário!"))
 }
 
-//AtualizandoUsuario altera as informações de um usuário no banco de dados
+// AtualizandoUsuario altera as informações de um usuário no banco de dados
 func AtualizarUsuario(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Atualizando Usuário!"))
 }
 
-//DeletarUsuario Exclui um usuário do banco de dados
+// DeletarUsuario Exclui um usuário do banco de dados
 func DeletarUsuario(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Deletando Usuário!"))
 }
