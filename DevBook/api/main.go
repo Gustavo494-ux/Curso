2326e5e70a4385673d302f00db1e6f78059d3c82
Gustavo Gama <gustavogama494@gmail.com>
2022-12-15 19:52:50 -0300

    89 - Gerando Chave Secret

--- DevBook/api/main.go
+++ DevBook/api/main.go
@@ -8,9 +8,26 @@ import (
 	"net/http"
 )
 
+/*
+func init() {
+	chave := make([]byte, 64)
+	fmt.Println(chave)
+
+	if _, erro := rand.Read(chave); erro != nil {
+		log.Fatal(erro)
+	}
+
+	stringBase64 := base64.StdEncoding.EncodeToString(chave)
+	fmt.Println(chave)
+	fmt.Println(stringBase64)
+}
+*/
+
 func main() {
 	config.Carregar()
 	fmt.Printf("Escutando na porta %d", config.Porta)
+
+	fmt.Println(config.SecretKey)
 
 	r := router.Gerar()
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
