26ff1f9e15ac96985a4ed9a7708c544fa69b80ed
Gustavo Gama <gustavogama494@gmail.com>
2023-05-03 20:32:45 -0300

    104. Busca de Publicação por ID

--- DevBook/api/src/repositorios/publicacoes.go
+++ DevBook/api/src/repositorios/publicacoes.go
@@ -14,6 +14,7 @@ func NovoRepositorioDePublicacoes(db *sql.DB) *Publicacoes {
 	return &Publicacoes{db}
 }
 
+// Criar cria uma nova publicação
 func (repositorio Publicacoes) Criar(publicacao modelos.Publicacao) (uint64, error) {
 	statement, erro := repositorio.db.Prepare(" insert into publicacoes (titulo,conteudo,autor_id) values (?,?,?) ")
 	if erro != nil {
@@ -31,4 +32,38 @@ func (repositorio Publicacoes) Criar(publicacao modelos.Publicacao) (uint64, err
 		return 0, erro
 	}
 	return uint64(ultimoIDInserido), nil
+}
+
+// BuscarPorID traz uma publicacao
+func (repositorio Publicacoes) BuscarPorID(publicacaoID uint64) (modelos.Publicacao, error) {
+	linha, erro := repositorio.db.Query(`
+		Select 
+			p.*, 
+			u.nick 
+		from devbook.publicacoes p 
+			inner join devbook.usuarios u on u.id = p.autor_id 
+		where 
+			p.id = ? `,
+		publicacaoID,
+	)
+	if erro != nil {
+		return modelos.Publicacao{}, erro
+	}
+	defer linha.Close()
+
+	var publicacao modelos.Publicacao
+	if linha.Next() {
+		if erro = linha.Scan(
+			&publicacao.ID,
+			&publicacao.Titulo,
+			&publicacao.Conteudo,
+			&publicacao.AutorID,
+			&publicacao.Curtidas,
+			&publicacao.CriadaEm,
+			&publicacao.AutorNick,
+		); erro != nil {
+			return modelos.Publicacao{}, erro
+		}
+	}
+	return publicacao, nil
 }
