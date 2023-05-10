0f1f9819b8aaaed666aa9598c4f7e28376804e37
Gustavo Gama <gustavogama494@gmail.com>
2023-05-09 21:15:04 -0300

    107. Exclusão de Publicação

--- DevBook/api/src/repositorios/publicacoes.go
+++ DevBook/api/src/repositorios/publicacoes.go
@@ -122,3 +122,20 @@ func (repositorio Publicacoes) AtualizarPublicacao(publicacaoID uint64, publicac
 
 	return nil
 }
+
+// DeletarPublicacao exclui os dados de uma publicação
+func (repositorio Publicacoes) DeletarPublicacao(publicacaoID uint64) error {
+	statement, erro := repositorio.db.Prepare(
+		` delete from publicacoes where id =? `,
+	)
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro = statement.Exec(publicacaoID); erro != nil {
+		return erro
+	}
+
+	return nil
+}
